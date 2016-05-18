7c97927b558fa7bde8d035e0b1a5d7557640cd21
charltonraven <williamscharlton@hotmail.com>
2016-05-18 16:06:12 -0400

    Added a method to check for an internet connection before fully starting when hitting the submit button

--- TestProjectsWord/CustomerInformation.cs
+++ TestProjectsWord/CustomerInformation.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
 
 namespace TestProjectsWord
 {
@@ -19,18 +20,36 @@ namespace TestProjectsWord
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            TransferInfo AddDataTransfer = new TransferInfo(txtNTName.Text, txtFirstName.Text, txtLastName.Text, txtPhone.Text, txtOldName.Text);
-            DatabaseAccess AddDT = new DatabaseAccess();
-            AddDT.Add(AddDataTransfer);
-            System.IO.File.WriteAllText(@"C:\cops\Number.txt", "Someone should be remoting in on your new PC in the next hour or less. If you do not see anyone on your new PC, Please give us a call at 843-383-7050. Thanks ");
-            System.Diagnostics.Process.Start(@"C:\cops\Number.txt");
+            //Ensure a connection before the summit button can go through
+            if (hasInternetConnection() == true)
+            {
+                TransferInfo AddDataTransfer = new TransferInfo(txtNTName.Text, txtFirstName.Text, txtLastName.Text, txtPhone.Text, txtOldName.Text);
+                DatabaseAccess AddDT = new DatabaseAccess();
+                AddDT.Add(AddDataTransfer);
+                System.IO.File.WriteAllText(@"C:\cops\Number.txt", "Someone should be remoting in on your new PC in the next hour or less. If you do not see anyone on your new PC, Please give us a call at 843-383-7050. Thanks ");
+                System.Diagnostics.Process.Start(@"C:\cops\Number.txt");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("NETWORK ERROR. Please check your network connection and Try Again");
+            }
 
-
-
-
-
-            this.Close();
-
+        }
+        //Chekcs for Internet Connection by ping a website(sonoco.com)...http://www.sonco.com would not work.
+        public bool hasInternetConnection()
+        {
+            string host = "sonoco.com";
+            bool result = false;
+            Ping p = new Ping();
+            try
+            {
+                PingReply reply = p.Send(host, 3000);
+                if (reply.Status == IPStatus.Success)
+                    return true;
+            }
+            catch { }
+            return result;
         }
     }
 }
